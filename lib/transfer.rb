d1cf3567bd341c98e1ea7db6f881cbbe35ca732b
Grace Kim <goooyoon@gmail.com>
2020-06-09 02:10:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,14 @@ attr_accessor :status, :sender, :receiver, :amount
   end
   
   def execute_transaction
-    
+    if self.valid? && @sender.balance > @amount && @status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status ="complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    end
   end
 end
